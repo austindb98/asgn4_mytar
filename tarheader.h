f924a8399a29f7d58f677cfb335128b6ba27ced4
Austin Bryant <austindbryant@gmail.com>
2019-02-24 15:56:19 -0800

    First code commit

---
+++ tarheader.h
@@ -0,0 +1,20 @@
+#include <stdint.h>
+
+typedef struct{
+	uint8_t name[100];
+	uint8_t mode[8];
+	uint8_t uid[8];
+	uint8_t gid[8];
+	uint8_t size[12];
+	uint8_t mtime[12];
+	uint8_t chksum[8];
+	uint8_t typeflag[1];
+	uint8_t linkname[100];
+	uint8_t magic[6];
+	uint8_t version[2];
+	uint8_t uname[32];
+	uint8_t gname[32];
+	uint8_t devmajor[8];
+	uint8_t devminor[8];
+	uint8_t prefix[155];
+} header;
