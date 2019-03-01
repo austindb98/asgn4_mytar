e04850cec8fa365fe5ae1b73521db860dcf17f65
Austin Bryant <austindbryant@gmail.com>
2019-03-01 08:52:50 -0800

    header printing

--- tarextract2.c
+++ tarextract2.c
@@ -40,6 +40,61 @@ void makedir(char *path, mode_t mode) {
     }
 }
 
+void printheader(header *fileheader) {
+    uint8_t buf[256];
+    memset(buf,'\0',256);
+    memcpy(buf,fileheader->name,100);
+    printf("name: %s\n",buf);
+    memset(buf,'\0',256);
+    memcpy(buf,fileheader->mode,8);
+    printf("mode: %s\n",buf);
+    memset(buf,'\0',256);
+    memcpy(buf,fileheader->uid,8);
+    printf("uid: %s\n",buf);
+    memset(buf,'\0',256);
+    memcpy(buf,fileheader->gid,100);
+    printf("gid: %s\n",buf);
+    memset(buf,'\0',256);
+    memcpy(buf,fileheader->size,12);
+    printf("size: %s\n",buf);
+    memset(buf,'\0',256);
+    memcpy(buf,fileheader->mtime,12);
+    printf("mtime: %s\n",buf);
+    memset(buf,'\0',256);
+    memcpy(buf,fileheader->chksum,8);
+    printf("chksum: %s\n",buf);
+    memset(buf,'\0',256);
+    memcpy(buf,fileheader->typeflag,1);
+    printf("typeflag: %s\n",buf);
+    memset(buf,'\0',256);
+    memcpy(buf,fileheader->linkname,100);
+    printf("linkname: %s\n",buf);
+    memset(buf,'\0',256);
+    memcpy(buf,fileheader->magic,6);
+    printf("magic: %s\n",buf);
+    memset(buf,'\0',256);
+    memcpy(buf,fileheader->version,2);
+    printf("version: %s\n",buf);
+    memset(buf,'\0',256);
+    memcpy(buf,fileheader->uname,32);
+    printf("uname: %s\n",buf);
+    memset(buf,'\0',256);
+    memcpy(buf,fileheader->gname,32);
+    printf("gname: %s\n",buf);
+    memset(buf,'\0',256);
+    memcpy(buf,fileheader->devmajor,8);
+    printf("devmajor: %s\n",buf);
+    memset(buf,'\0',256);
+    memcpy(buf,fileheader->devminor,8);
+    printf("devminor: %s\n",buf);
+    memset(buf,'\0',256);
+    memcpy(buf,fileheader->prefix,155);
+    printf("prefix: %s\n",buf);
+    memset(buf,'\0',256);
+    memcpy(buf,fileheader->null,12);
+    printf("null?: %s\n",buf);
+}
+
 char *makepath(header *fileheader) {
     if(*(fileheader->prefix)) {
         char *out = calloc(1,256);
@@ -111,7 +166,7 @@ int extract(char *archivename) {
     char * path;
     /*directory check*/
     while(read(fdTar, &fileheader, 512) == 512) {
-
+        printheader(&fileheader);
         /*if(validateheader(&fileheader)) {
             fprintf(stderr, "---BAD HEADER---\n");
         }*/
