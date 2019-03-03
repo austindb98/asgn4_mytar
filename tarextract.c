cade831f5d7b5fa346c7425a97c619c868a4735b
Austin Bryant <austindbryant@gmail.com>
2019-03-02 20:29:20 -0800

    format

--- tarextract.c
+++ tarextract.c
@@ -119,8 +119,8 @@ int extract(char *archivename, char **targets, int numtargets) {
                 }
                 targetflag = !strncmp(path,targets[i],length);
             } else {
-                fprintf(stderr,"Length: %s \tPath: %s",strlen(path),path);
-                fprintf(stderr,"Length: %s \tTarget: %s",
+                fprintf(stderr,"Length: %d \tPath: %s",strlen(path),path);
+                fprintf(stderr,"Length: %d \tTarget: %s",
                         strlen(targets[i]),targets[i]);
             }
         }
