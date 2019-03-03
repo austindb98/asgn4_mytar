7ca38e8f3da376a37b98f01794f6a82c8ba37765
Austin Bryant <austindbryant@gmail.com>
2019-03-02 17:54:26 -0800

    remove printspam

--- tarutil.c
+++ tarutil.c
@@ -151,12 +151,11 @@ int validateheader(header *fileheader) {
     for(i = 0; i < 512; i++) {
         newchksum += byteptr[i];
     }
+    /*
     fprintf(stderr,"Actual sum (source): %s\n", fileheader->chksum);
-    //fprintf(stdout,"Actual sum (copy): %s\n", chksum);
-
     snprintf(newsumstr, 8, "%0*o", 7, newchksum);
     fprintf(stderr,"Calculated sum: %s\n", newsumstr);
-
+    */
     int out;
     if(chksum[0] != '0') {
         out = 0;
