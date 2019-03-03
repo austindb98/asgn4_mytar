0db8442b3283c73268a9658fe28e4d0465d1c85f
Austin Bryant <austindbryant@gmail.com>
2019-03-02 20:43:12 -0800

    placement

--- tarextract.c
+++ tarextract.c
@@ -116,7 +116,7 @@ int extract(char *archivename, char **targets, int numtargets) {
                 targetflag = !strcmp(path,targets[i]);
             } else {
                 fprintf(stderr,"Length: %d \tPath: %s\n",strlen(path),path);
-                fprintf(stderr,"Length: %d \tTarget: %s\n\n",
+                fprintf(stderr,"Length: %d \tTarget: %s\n",
                         strlen(targets[i]),targets[i]);
                 int length = strlen(path);
                 if(strlen(targets[i]) < length) {
@@ -126,6 +126,8 @@ int extract(char *archivename, char **targets, int numtargets) {
                     fprintf(stderr, "Comparing over length of path\n");
                     targetflag = !strcmp(path,targets[i]);
                 }
+                targetflag = !strncmp(path,targets[i],length);
+                printf("%d", targetflag);
             }
         }
 
